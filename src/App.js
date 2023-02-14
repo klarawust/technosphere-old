72609a8ab94f9ef5cff11311f13b2f6b7a7c7487
Klara Wüstenberg <klarawust@gmail.com>
2023-02-14 19:08:06 +0100

    forsøk på redning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./components/List";
 
 function App() {
+  const venner = ["Tale", "Aurora", "Frida", "Johanna"];
+  const bikkjer = ["Labradoodle", "Aurora", "Frida", "Johanna"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List entries={venner} title="Venner" />
+      <List entries={bikkjer} title="Bikkjene mine" />
     </div>
   );
 }
