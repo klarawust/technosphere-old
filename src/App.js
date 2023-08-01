ae03e8cbbb325a9cbb51679649e8fe3f972d081e
Klara Wüstenberg <klarawust@gmail.com>
2023-08-01 14:55:33 +0200

    add more scalability

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ function App() {
   return (
     <main>
       <Navbar />
-      <div className="grid grid-cols-8">
-        <div className="col-span-6 font-mono mx-[10vw]">
+      <div className="sm:grid grid-cols-8">
+        <div className="col-span-8 sm:col-span-6 font-mono mx-[10vw]">
           <Hjem />
           <Meg />
           <Prosjekter />
         </div>
-        <div className="col-span-1">
+        <div className="col-span-1 font-mono">
+          <h1 className="sm:hidden text-4xl text-sky-600 mx-[10vw]">BILDER</h1>
           <Bildekant />
         </div>
       </div>
