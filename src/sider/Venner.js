d276ab3424491d5e516a88db55ece6396b2c7264
Klara Wüstenberg <klarawust@gmail.com>
2023-11-14 09:41:12 +0200

    Add Aurora as friend:)

--- src/sider/Venner.js
+++ src/sider/Venner.js
@@ -5,12 +5,12 @@ import isak from "../images/venner/isak_1280.webp";
 import magnus from "../images/venner/magnus_1280.webp";
 import nuutti from "../images/venner/nuutti_1280.webp";
 import mattias from "../images/venner/mattias_1280.webp";
+import aurora from "../images/venner/aurora_1280.webp";
 
 function Venner() {
   const venner = [
-    // { name: "Aurora", url: "", bilde: aurora },
-    // { name: "Mikkel", url: "", bilde: mikkel },
     { name: "Isak", url: "https://isak.me/", bilde: isak },
+    { name: "Aurora", url: "https://kennedy.no", bilde: aurora },
     { name: "Magnus", url: "https://www.magnus.ouren.no", bilde: magnus },
     { name: "Nuutti", url: "https://nuutti.no", bilde: nuutti },
     {
